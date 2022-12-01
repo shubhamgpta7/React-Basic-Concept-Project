47ad0c8fc5233e7c2f014161a545f4f1cc94394f
shubhamgpta7 <shubhamgpta7@gmail.com>
2022-12-01 17:32:28 +0545

    Wrapper, Fragments & Refs in React

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 
 import Card from "../UI/Card";
@@ -47,7 +48,7 @@ const AddUser = props => {
         }
 
     return(
-        <div>
+        <Wrapper>
         {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
         
         <Card className={classes.input}>
@@ -60,7 +61,7 @@ const AddUser = props => {
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
-        </div>
+        </Wrapper>
     );
 };
 
